69fe2bfad98d5e82851eab56b4e11290cf591191
Mayuka Jayawardhana <mayukaj@nexuscml00.umiacs.umd.edu>
2023-01-19 15:35:22 -0500

    Add RPE support

--- timm/models/vision_transformer.py
+++ timm/models/vision_transformer.py
@@ -42,6 +42,7 @@ from ._builder import build_model_with_cfg
 from ._manipulate import named_apply, checkpoint_seq, adapt_input_conv
 from ._pretrained import generate_default_cfgs
 from ._registry import register_model
+from timm.utils import build_rpe, get_rpe_config
 
 
 __all__ = ['VisionTransformer']  # model_registry will add each entrypoint fn to this
@@ -73,6 +74,61 @@ class Attention(nn.Module):
         attn = self.attn_drop(attn)
 
         x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+
+class RPEAttention(nn.Module):
+    '''
+    Attention with image relative position encoding
+    '''
+
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., rpe_config=None):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+        # image relative position encoding
+        self.rpe_q, self.rpe_k, self.rpe_v = \
+            build_rpe(rpe_config,
+                      head_dim=head_dim,
+                      num_heads=num_heads)
+
+    def forward(self, x):
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]   # make torchscript happy (cannot use tensor as tuple)
+
+        q *= self.scale
+
+        attn = (q @ k.transpose(-2, -1))
+
+        # image relative position on keys
+        if self.rpe_k is not None:
+            attn += self.rpe_k(q)
+
+        # image relative position on queries
+        if self.rpe_q is not None:
+            attn += self.rpe_q(k * self.scale).transpose(2, 3)
+
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        out = attn @ v
+
+        # image relative position on values
+        if self.rpe_v is not None:
+            out += self.rpe_v(attn)
+
+        x = out.transpose(1, 2).reshape(B, N, C)
         x = self.proj(x)
         x = self.proj_drop(x)
         return x
@@ -118,6 +174,26 @@ class Block(nn.Module):
     def forward(self, x):
         x = x + self.drop_path1(self.ls1(self.attn(self.norm1(x))))
         x = x + self.drop_path2(self.ls2(self.mlp(self.norm2(x))))
+        return x
+
+
+class RPEBlock(nn.Module):
+
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, rpe_config=None):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = RPEAttention(
+            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop, rpe_config=rpe_config)
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+
+    def forward(self, x):
+        x = x + self.drop_path(self.attn(self.norm1(x)))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
         return x
 
 
@@ -465,6 +541,90 @@ class VisionTransformerPEG(VisionTransformer):
         else:
             x = self.blocks(x)
         x = self.norm(x)
+        return x
+
+
+class VisionTransformerRPE(nn.Module):
+    """ Vision Transformer with support for patch or hybrid CNN input stage
+                           and image relative position encoding
+    """
+
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=768, depth=12,
+                 num_heads=12, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0.,
+                 drop_path_rate=0., hybrid_backbone=None, norm_layer=nn.LayerNorm, rpe_config=None):
+        super().__init__()
+        self.num_classes = num_classes
+        self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models
+
+        # if hybrid_backbone is not None:
+        #     self.patch_embed = HybridEmbed(
+        #         hybrid_backbone, img_size=img_size, in_chans=in_chans, embed_dim=embed_dim)
+        # else:
+        self.patch_embed = PatchEmbed(
+            img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
+        num_patches = self.patch_embed.num_patches
+
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim))
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  # stochastic depth decay rule
+        self.blocks = nn.ModuleList([
+            RPEBlock(
+                dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer, rpe_config=rpe_config)
+            for i in range(depth)])
+        self.norm = norm_layer(embed_dim)
+
+        # NOTE as per official impl, we could have a pre-logits representation dense layer + tanh here
+        #self.repr = nn.Linear(embed_dim, representation_size)
+        #self.repr_act = nn.Tanh()
+
+        # Classifier head
+        self.head = nn.Linear(embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+
+        trunc_normal_(self.pos_embed, std=.02)
+        trunc_normal_(self.cls_token, std=.02)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'pos_embed', 'cls_token'}
+
+    def get_classifier(self):
+        return self.head
+
+    def reset_classifier(self, num_classes, global_pool=''):
+        self.num_classes = num_classes
+        self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+
+    def forward_features(self, x):
+        B = x.shape[0]
+        x = self.patch_embed(x)
+
+        cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+        x = torch.cat((cls_tokens, x), dim=1)
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        for blk in self.blocks:
+            x = blk(x)
+
+        x = self.norm(x)
+        return x[:, 0]
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = self.head(x)
         return x
 
 
@@ -1695,3 +1855,148 @@ def vit_small_patch16_64(pretrained=False, **kwargs):
     model_kwargs = dict(patch_size=16, embed_dim=384, depth=12, num_heads=6, **kwargs)
     model = _create_vision_transformer('vit_small_patch16_64', pretrained=pretrained, **model_kwargs)
     return model
+
+@register_model
+def deit_small_patch16_224(pretrained=False, **kwargs):
+    model = VisionTransformerRPE(
+        patch_size=16, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/deit/deit_small_patch16_224-cd65a155.pth",
+            map_location="cpu", check_hash=True
+        )
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+
+_checkpoint_url_prefix = \
+    'https://github.com/wkcn/iRPE-model-zoo/releases/download/1.0/'
+_provided_checkpoints = set([
+    'deit_tiny_patch16_224_ctx_product_50_shared_k',
+    'deit_small_patch16_224_ctx_product_50_shared_k',
+    'deit_small_patch16_224_ctx_product_50_shared_qk',
+    'deit_small_patch16_224_ctx_product_50_shared_qkv',
+    'deit_base_patch16_224_ctx_product_50_shared_k',
+    'deit_base_patch16_224_ctx_product_50_shared_qkv',
+])
+
+def register_rpe_model(fn):
+    '''Register a model with iRPE
+    It is a wrapper of `register_model` with loading the pretrained checkpoint.
+    '''
+    def fn_wrapper(pretrained=False, **kwargs):
+        model = fn()
+        if pretrained:
+            model_name = fn.__name__
+            assert model_name in _provided_checkpoints, \
+                f'Sorry that the checkpoint `{model_name}` is not provided yet.'
+            url = _checkpoint_url_prefix + model_name + '.pth'
+            checkpoint = torch.hub.load_state_dict_from_url(
+                url=url,
+                map_location='cpu', check_hash=False,
+            )
+            model.load_state_dict(checkpoint['model'])
+
+        return model
+
+    # rename the name of fn_wrapper
+    fn_wrapper.__name__ = fn.__name__
+    return register_model(fn_wrapper)
+
+@register_rpe_model
+def deit_small_patch16_224_ctx_euc_20_shared_k(pretrained=False, **kwargs):
+    # DeiT-Small with relative position encoding on keys (Contextual Euclidean method)
+    rpe_config = get_rpe_config(
+        ratio=20,
+        method="euc",
+        mode='ctx',
+        shared_head=True,
+        skip=1,
+        rpe_on='k',
+    )
+    return deit_small_patch16_224(pretrained=pretrained,
+                                  rpe_config=rpe_config,
+                                  **kwargs)
+
+
+@register_rpe_model
+def deit_small_patch16_224_ctx_quant_51_shared_k(pretrained=False, **kwargs):
+    # DeiT-Small with relative position encoding on keys (Contextual Quantization method)
+    rpe_config = get_rpe_config(
+        ratio=33,
+        method="quant",
+        mode='ctx',
+        shared_head=True,
+        skip=1,
+        rpe_on='k',
+    )
+    return deit_small_patch16_224(pretrained=pretrained,
+                                  rpe_config=rpe_config,
+                                  **kwargs)
+
+
+@register_rpe_model
+def deit_small_patch16_224_ctx_cross_56_shared_k(pretrained=False, **kwargs):
+    # DeiT-Small with relative position encoding on keys (Contextual Cross method)
+    rpe_config = get_rpe_config(
+        ratio=20,
+        method="cross",
+        mode='ctx',
+        shared_head=True,
+        skip=1,
+        rpe_on='k',
+    )
+    return deit_small_patch16_224(pretrained=pretrained,
+                                  rpe_config=rpe_config,
+                                  **kwargs)
+
+
+@register_rpe_model
+def deit_small_patch16_224_ctx_product_50_shared_k(pretrained=False, **kwargs):
+    # DeiT-Small with relative position encoding on keys (Contextual Product method)
+    rpe_config = get_rpe_config(
+        ratio=1.9,
+        method="product",
+        mode='ctx',
+        shared_head=True,
+        skip=1,
+        rpe_on='k',
+    )
+    return deit_small_patch16_224(pretrained=pretrained,
+                                  rpe_config=rpe_config,
+                                  **kwargs)
+
+
+@register_rpe_model
+def deit_small_patch16_224_ctx_product_50_shared_qk(pretrained=False, **kwargs):
+    # DeiT-Small with relative position encoding on queries and keys (Contextual Product method)
+    rpe_config = get_rpe_config(
+        ratio=1.9,
+        method="product",
+        mode='ctx',
+        shared_head=True,
+        skip=1,
+        rpe_on='qk',
+    )
+    return deit_small_patch16_224(pretrained=pretrained,
+                                  rpe_config=rpe_config,
+                                  **kwargs)
+
+
+@register_rpe_model
+def deit_small_patch16_224_ctx_product_50_shared_qkv(pretrained=False, **kwargs):
+    # DeiT-Small with relative position encoding on queries, keys and values (Contextual Product method)
+    rpe_config = get_rpe_config(
+        ratio=1.9,
+        method="product",
+        mode='ctx',
+        shared_head=True,
+        skip=1,
+        rpe_on='qkv',
+    )
+    return deit_small_patch16_224(pretrained=pretrained,
+                                  rpe_config=rpe_config,
+                                  **kwargs)
+
